4d3af0f73675d7398fc824f52dd8cd61c50d154f
Anna Bladey <abladey@civisanalytics.com>
2021-03-26 14:47:05 -0700

    ENH: add example of testable pipeline architecture

---
+++ testable_architecture/README.md
@@ -0,0 +1,4 @@
+# Architecting Testable Pipelines
+
+* Data processing pipelines must be testable so that they can be maintained with stability and accuracy
+* Parameterize all references to code and data to facilitate switching between development and production “environments”
---
+++ testable_architecture/script_1.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+import civis
+
+
+def main(input_filepath, intermediary_tbname, db_name):
+    """
+    Transform input data and output to intermediary table.
+
+    Parameters:
+    ------------
+    input_filepath: str
+        Filepath of a csv containing input data.
+    intermediary_tbname: str
+        schema.tablename where intermediary data
+        will be written.
+    db_name: str
+        Name of the database on which `intermediary_tbname`
+        will be written.
+
+    Returns:
+    ---------
+    None
+
+    """
+
+    input_df = pd.read_csv(input_filepath)
+    print(f"Read input file {input_filepath}:\n{input_df}")
+
+    intermediary_df = input_df * 2
+    print(f"Intermediary data:\n{intermediary_df}")
+
+    civis.io.dataframe_to_civis(
+        df=intermediary_df,
+        table=intermediary_tbname,
+        database=db_name,
+        existing_table_rows="drop",
+    )
+    print(
+        f"Wrote intermediary data to table {intermediary_tbname} on database {db_name}."
+    )
+
+
+if __name__ == "__main__":
+    main(
+        input_filepath=os.environ["INPUT_FILEPATH"],
+        intermediary_tbname=os.environ["INTERMEDIARY_TBNAME"],
+        db_name=os.environ["DB_NAME"],
+    )
