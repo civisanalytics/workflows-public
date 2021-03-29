86ccf5bfe37d9b3e06d7b7aa69e84abb5e66a53f
Anna Bladey <abladey@civisanalytics.com>
2021-03-29 07:55:03 -0700

    print statements and docstrings

--- testable_architecture/script_1.py
+++ testable_architecture/script_1.py
@@ -3,9 +3,30 @@ import pandas as pd
 
 
 def main(input_data_reference, intermediary_data_reference):
+    """
+    Transform input data and output to intermediary location.
+
+    Parameters:
+    ------------
+    input_data_reference: str
+        Filepath of a csv containing input data.
+    intermediary_data_reference: str
+        Filepath where a csv containing intermediary data
+        will be written.
+
+    Returns:
+    ---------
+    None
+
+    """
+
+    print(f'Reading input file {input_data_reference}.')
     input_df = pd.read_csv(input_data_reference)
+
     intermediary_df = input_df * 2
+
     intermediary_df.to_csv(intermediary_data_reference, index=False)
+    print(f'Wrote intermediary file {intermediary_data_reference}.')
 
 
 if __name__ == "__main__":
--- testable_architecture/script_2.py
+++ testable_architecture/script_2.py
@@ -3,9 +3,30 @@ import pandas as pd
 
 
 def main(intermediary_data_reference, output_data_reference):
+    """
+    Transform intermediary data and output to final destination.
+
+    Parameters:
+    ------------
+    intermediary_data_reference: str
+        Filepath of a csv containing intermediary data.
+    output_data_reference: str
+        Filepath where a csv containing output data
+        will be written.
+
+    Returns:
+    ---------
+    None
+
+    """
+
+    print(f'Reading intermediary file {intermediary_data_reference}.')
     intermediary_df = pd.read_csv(intermediary_data_reference)
+
     output_df = intermediary_df * 10
+
     output_df.to_csv(output_data_reference, index=False)
+    print(f'Wrote output file {output_data_reference}.')
 
 
 if __name__ == "__main__":
