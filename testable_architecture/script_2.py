553a7e9a52e7fc5e4722ec9f5bc8adc4eae99031
Nik (Anna) Bladey <anna.bladey@gmail.com>
2021-03-29 14:35:43 -0700

    ENH: add example of testable pipeline architecture (#8)

---
+++ testable_architecture/script_2.py
@@ -0,0 +1,45 @@
+import os
+import civis
+
+
+def main(intermediary_tbname, output_tbname, db_name):
+    """
+    Transform intermediary data and output to final destination.
+
+    Parameters:
+    ------------
+    intermediary_tbname: str
+        schema.tablename containing intermediary data.
+    output_tbname: str
+        Filepath where a csv containing output data
+        will be written.
+    db_name: str
+        Name of the database on which `intermediary_tbname`
+        exists and `output_tbname` will be written.
+
+    Returns:
+    ---------
+    None
+
+    """
+
+    intermediary_df = civis.io.read_civis(
+        table=intermediary_tbname, database=db_name, use_pandas=True
+    )
+    print(f"Read intermediary table {intermediary_tbname}:\n{intermediary_df}")
+
+    output_df = intermediary_df * 10
+    print(f"Output data:\n{output_df}")
+
+    civis.io.dataframe_to_civis(
+        df=output_df, table=output_tbname, database=db_name, existing_table_rows="drop",
+    )
+    print(f"Wrote output data to table {output_tbname} on database {db_name}.")
+
+
+if __name__ == "__main__":
+    main(
+        intermediary_tbname=os.environ["INTERMEDIARY_TBNAME"],
+        output_tbname=os.environ["OUTPUT_TBNAME"],
+        db_name=os.environ["DB_NAME"],
+    )
